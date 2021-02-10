16b90b10a031545226029a4097c07682eb091f69
Daniel Ahn <iamdanahn@gmail.com>
2021-02-10 11:42:12 -0500

    render bar change and add value to bars

--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,6 @@
 // Baseline Nutrition Facts
 // Baseline 100%,2000,75,20,0,300,2300,275,28,0,0
 
-// d3.csv("../assets/NutritionFacts.csv").then((d) => {
-// data.forEach((d) => {
-//   d.Calories = +d.Calories;
-//   d["Total Fat (g)"] = +d["Total Fat (g)"];
-//   d["Saturated Fat (g)"] = +d["Saturated Fat (g)"];
-//   d["Trans Fat (g)"] = +d["Trans Fat (g)"];
-//   d["Cholesterol (mg)"] = +d["Cholesterol (mg)"];
-//   d["Sodium (mg)"] = +d["Sodium (mg)"];
-//   d["Total Carbohydrates (g)"] = +d["Total Carbohydrates (g)"];
-//   d["Dietary Fiber (g)"] = +d["Dietary Fiber (g)"];
-//   d.Sugars = +d.Sugars;
-//   d.Protein = +d.Protein;
-// });
-// }
-
 d3.csv("../assets/NutritionFacts.csv", (d) => {
   return {
     type: d.Ramen,
@@ -35,72 +20,70 @@ d3.csv("../assets/NutritionFacts.csv", (d) => {
   console.log(data)
 })
 
-const width = 800
-const height = 500
-const margin = { top: 50, bottom: 50, left: 50, right: 50 }
-
-// creates svg element
-const svg = d3
-  .select(".d3-ramen")
-  .append("svg") // adds svg ele as child to d3-div
-  .attr("viewBox", [0, 0, width, height]) // creates the SVG box
-  .attr("height", height - margin.top - margin.bottom) // chart height
-  .attr("width", width - margin.left - margin.right) // chart width
-
+// data xfer from d4.csv promise
 const render = (data) => {
-  // const tip = d3
-  //   .tip()
-  //   .attr("class", "d3-tip")
-  //   .html(function (d) {
-  //     return d.type
-  //   })
+  const width = 800
+  const height = 650
+  const margin = { top: 50, bottom: 150, left: 50, right: 50 }
 
-  // creates x axis boundaries
+  // creates x scale
   const x = d3
-    .scaleBand()
-    .domain(d3.range(data.length)) // dynamic range of elements
+    .scaleBand() // creates "bands"/columns
     .range([margin.left, width - margin.right]) // x axis range
     .padding(0.1) // reg css padding
+    .domain(data.map((d) => d.type)) //d3.range(data.length)) // dynamic range of elements
 
-  // create y axis boundaries
+  // create y scale
   const y = d3
     .scaleLinear() // scales your data to range size
-    .domain([0, d3.max(data, (d) => d.calories)]) // y axis scaling
     .range([height - margin.bottom, margin.top]) // y axis range
+    .domain([0, d3.max(data, (d) => d.calories)]) // y axis scaling
+
+  // creates svg element
+  const svg = d3
+    .select(".d3-ramen")
+    .append("svg") // adds svg ele as child to d3-div
+    .attr("viewBox", [0, 0, width, height]) // creates the SVG box
+    .attr("preserveAspectRatio", "xMidYMid meet") // preserves aspect ratio
+    .attr("height", height - margin.top - margin.bottom) // chart height
+    .attr("width", width - margin.left - margin.right) // chart width
 
   // creates the bars
-  svg
+  const bar = svg
     .append("g") // adds g ele to svg ele
     .attr("fill", "lightgreen")
+    .attr("mix-blend-mode", "multiply")
     .selectAll("rect") // selects all rectangles, if none found, creates empty selection
     .data(data) //.sort((a, b) => d3.descending(a.calories, b.calories)))
-    .join("rect")
-    .attr("class", "rectangle") // adds classname, now can be modified in scss
-    .attr("mix-blend-mode", "multiply")
-    .attr("x", (d, i) => x(i)) // places elements in order on x axis, d=data, i=index
+    .join("rect") // similar to .join((enter) => enter.append('rect'))
+    .attr("class", "bar") // adds classname, now can be modified in scss
+    .attr("x", (d) => x(d.type)) //, i) => x(i)) // places elements in order on x axis, d=data, i=index
     .attr("y", (d) => y(d.calories))
     .attr("height", (d) => y(0) - y(d.calories))
     .attr("width", x.bandwidth()) // calcs thickness of bars
+    .attr("value", (d, i) => d.type)
 
-  // shows label on bottom
+  // label on bottom
   function xAxis(g) {
     debugger
-    g.call(d3.axisBottom(x).tickFormat((i) => data[i].type)) //
+    g.call(d3.axisBottom(x))
       .attr("class", "xText")
       .attr("font-size", "20px") // original font was too small
       .attr("transform", `translate(0, ${height - margin.bottom})`) // x-axis line was showing on top
       .selectAll("text")
-      .attr("transform", "rotate(-45)")
+      .attr("transform", "rotate(-25)")
+      .attr("text-anchor", "end")
   }
 
+  // label on left
   function yAxis(g) {
-    g.call(d3.axisLeft(y).ticks(null, data.format))
+    g.call(d3.axisLeft(y)) //.ticks(null, data.format))
       .attr("font-size", "15px")
       .append("text")
       .attr("class", "yText")
   }
 
-  d3.select(".sorter").on("change", sort)
+  d3.select("input").on("change", sort)
 
   // const sortTimeout = setTimeout(() => {
   //   debugger
@@ -108,7 +91,7 @@ const render = (data) => {
   // }, 500)
 
   function sort() {
-    clearTimeout(sortTimeout)
+    // clearTimeout(sortTimeout)
 
     debugger
     const newX = x
