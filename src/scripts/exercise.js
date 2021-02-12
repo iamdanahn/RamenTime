2f63f0cb88cff11e7d7b7c36f204b367370539d0
Daniel Ahn <iamdanahn@gmail.com>
2021-02-12 10:55:38 -0500

    working on side chart

--- src/scripts/exercise.js
+++ src/scripts/exercise.js
@@ -1,82 +1,122 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  d3.csv("../../assets/exercise.csv", (d) => {
-    debugger
-    return {
-      exercise: d.Exercise,
-      calories: +d.Calories,
-    }
-  }).then((data) => {
-    console.log(data)
-    const margin = { top: 50, bottom: 150, left: -50, right: 10 }
-    const width = 400
-    const height = 650
+  
+  const margin = { top: 50, bottom: 150, left: 10, right: 10 }
+  const width = 400
+  const height = 650
 
-    const x = d3
-      .scaleBand()
-      .domain(data.map((d) => d.exercise))
-      .range([0, width - margin.right])
-      .padding(0.1)
-
-    const y = d3
-      .scaleLinear()
-      .domain([0, 15])
-      .nice()
-      .range([height - margin.bottom, margin.top])
-
-    function xAxis(g) {
-      g.call(d3.axisBottom(x))
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .attr("class", "xAxis exercise")
-        .selectAll("text")
-        .attr("transform", "rotate(-25)")
-    }
-
-    function yAxis(g) {
-      g.call(d3.axisRight(y))
-        .attr("class", "yAxis exercise")
-        .attr("transform", `translate(${width} ,0)`) // puts axis on right side
-    }
-
-    // tooltip popup
-    // d3-tip event handlers rcv "event" as 1st arg
-    //
-    const tip = d3
-      .tip()
-      .attr("class", "d3-tip exercise")
-      .html((event, d) => {
-        return d.calories
-      })
-
-    const svg = d3
-      .select(".d3-exercise")
-      .append("svg")
+  const svg = d3
+    .select(".d3-exercise")
+    .append("svg")
       .attr("viewBox", [0, 0, width, height])
       .attr("preserveAspectRatio", "xMidYMid meet")
       .attr("height", height - margin.top - margin.bottom) // chart height
       .attr("width", width - margin.left - margin.right) // chart width
 
-    const bars = svg
-      .append("g")
+  const x = d3.scaleBand()
+    .range([0, width - margin.right])
+    .padding(0.1)
+  const xAxis = svg
+    .append("g")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    
+  const y = d3.scaleLinear()
+    .range([height - margin.bottom, margin.top])
+  const yAxis = svg
+    .append("g")
+    .attr("class", "yAxis exercise")
+    .attr("transform", `translate(${width - margin.right}, 0)`)
 
-      .selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("class", "bar exercise")
-      .attr("x", (d) => x(d.exercise)) // spreads bars over x axis
-      .attr("y", (d) => y(d.calories))
-      .attr("height", (d) => y(0) - y(d.calories))
-      .attr("width", x.bandwidth())
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide)
+  function update(time) {
+    d3.csv("../../assets/exercise.csv", function(data) {
+      debugger
+      x.domain(data.map((d) => d.Exercise))
+      xAxis.transition().duration(1000).call(d3.axisBottom(x))
+        .attr("class", "xAxis exercise")
+        .selectAll("text")
+        .attr("transform", "rotate(-25)")
+      
+      y.domain([0, d3.max(data, (d) => +d.Calories)]).nice()
+      yAxis.transition().duration(1000).call(d3.axisRight(y))
 
-    // call area to render features
-    svg.call(tip)
-    svg.append("g").call(xAxis)
-    svg.append("g").call(yAxis)
-    return svg.node()
-  })
+      const tip = d3
+        .select("body")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "d3-tip exercise")
+
+      // when 
+      const u = svg.selectAll("rect").data(data)
+      const e = d3.event
+
+      u.enter()
+        .append("rect")
+        .attr("class", "bar exercise")
+        .merge(u)
+        // .transition()
+        // .duration(1000)
+        .attr("x", (d) => x(d.Exercise))
+        .attr("y", (d) => y(d[time]))
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => y(0) - y(d[time]))
+        .on("mouseover", (d, i) => {
+          tip.transition().duration(300).style("opacity", 0.8)
+          console.log(d)
+          console.log(d3.event)
+          tip
+            .html(
+              `Exercise: ${d.Exercise} <br/>
+              Calories: ${+d.Calories} <br/>`,
+            )
+            .style("left", `${d3.event.clientX}px`)
+            .style("top", `${d3.event.clientY}px`)
+        })
+        .on("mousemove", (d) => {
+          // console.log(e)
+          // console.log(d)
+          tip
+            .style("left", `${d3.event.clientX}px`)
+            .style("top", `${d3.event.clientY - 50}px`)
+        })
+        .on("mouseout", (d) => {
+          // console.log(e)
+          // console.log(d)
+          tip.transition().duration(100).style("opacity", 0)
+        })
+
+      u.exit().remove()
+      })
+  }
+
+  d3.selectAll(".btn-exercise")
+    .on("click", function() {
+      update(this.value)
+    })
+
+  update('Calories')
 })
 
-document.addEventListener("click", (e) => {
-  console.log(e)
-})
+
+
+    // const bars = svg
+    //   .append("g")
+
+    //   .selectAll("rect")
+    //   .data(data)
+    //   .join("rect")
+    //   .attr("class", "bar exercise")
+    //   .attr("x", (d) => x(d.exercise)) // spreads bars over x axis
+    //   .attr("y", (d) => y(d.calories))
+    //   .attr("height", (d) => y(0) - y(d.calories))
+    //   .attr("width", x.bandwidth())
+    //   .on("mouseover", tip.show)
+    //   .on("mouseout", tip.hide)
+
+    // // call area to render features
+    // svg.call(tip)
+    // svg.append("g").call(xAxis)
+    // svg.append("g").call(yAxis)
+    // return svg.node()
+//   })
+// })
+
--- src/scripts/ramen.js
+++ src/scripts/ramen.js
@@ -1,29 +1,28 @@
 // Baseline Nutrition Facts
 // Baseline 100%,2000,75,20,0,300,2300,275,28,0,0
 
-document.addEventListener("DOMContentLoaded", () => {
-  d3.csv("../assets/nutrition_facts.csv", (d) => {
-    return {
-      brand: d.Brand,
-      type: d.Ramen,
-      calories: +d.Calories,
-      totalFat: +d["Total Fat (g)"],
-      sFat: +d["Saturated Fat (g)"],
-      tFat: +d["Trans Fat (g)"],
-      cholesterol: +d["Cholesterol (mg)"],
-      sodium: +d["Sodium (mg)"],
-      carbs: +d["Total Carbohydrates (g)"],
-      fiber: +d["Dietary Fiber (g)"],
-      sugars: +d.Sugars,
-      protein: +d.Protein,
-    }
-  }).then((data) => {
-    render(data)
-    console.log(data)
-  })
 
-  // data xfer from d4.csv promise
-  const render = (data) => {
+document.addEventListener("DOMContentLoaded", () => {
+  d3.csv("../assets/nutrition_facts.csv", function(data) {
+
+    data.forEach(d => {
+      debugger
+      return {
+        brand: d.Brand,
+        type: d.Ramen,
+        calories: +d.Calories,
+        totalFat: +d["Total Fat (g)"],
+        sFat: +d["Saturated Fat (g)"],
+        tFat: +d["Trans Fat (g)"],
+        cholesterol: +d["Cholesterol (mg)"],
+        sodium: +d["Sodium (mg)"],
+        carbs: +d["Total Carbohydrates (g)"],
+        fiber: +d["Dietary Fiber (g)"],
+        sugars: +d.Sugars,
+        protein: +d.Protein,
+      }
+    })
+
     const width = 700
     const height = 650
     const margin = { top: 50, bottom: 150, left: 20, right: 0 }
@@ -32,24 +31,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const svg = d3
       .select(".d3-ramen")
       .append("svg") // adds svg ele as child to d3-div
-      .attr("viewBox", [0, 0, width, height]) // creates the SVG box
-      .attr("preserveAspectRatio", "xMidYMid meet") // preserves aspect ratio
-      .attr("height", height - margin.top - margin.bottom) // chart height
-      .attr("width", width - margin.left - margin.right) // chart width
+        .attr("viewBox", [0, 0, width, height]) // creates the SVG box
+        .attr("preserveAspectRatio", "xMidYMid meet") // preserves aspect ratio
+        .attr("height", height - margin.top - margin.bottom) // chart height
+        .attr("width", width - margin.left - margin.right) // chart width
 
     // creates x scale
-    const x = d3
-      .scaleBand() // creates "bands"/columns
+    const x = d3.scaleBand() // creates "bands"/columns
       .range([margin.left / 2, width - margin.right]) // x axis range
       .padding(0.1) // reg css padding
       .domain(data.map((d) => d.type)) //d3.range(data.length)) // dynamic range of elements
 
     // create y scale
     const y = d3
+      
       .scaleLinear() // scales your data to range size
       .range([height - margin.bottom, margin.top]) // y axis range
       .domain([0, d3.max(data, (d) => d.calories)]) // y axis scaling
-      .nice() // adds remaining top tick if left out
+      .nice() // adds remaining top tick if left out, insert after domain
 
     // tooltip above bar
     const tip = d3
@@ -96,15 +95,18 @@ document.addEventListener("DOMContentLoaded", () => {
       })
 
       .attr("value", (d, i) => d.type)
-      .on("click", (d) => console.log(d.toElement.__data__.type))
+      .on("click", (e, d) => {
+        console.log(e.toElement.__data__.type)
+        console.log(d)
+        console.log(update)
+      })
 
-    // bind tip to the svg
-    // svg.call(tip)
+
 
     // label on bottom
     function xAxis(g) {
       debugger
-      g.call(d3.axisBottom(x))
+      g.call(d3.axisBottom(x)) // controls which side of the axis text shows
         .attr("class", "xText")
         .attr("font-size", "20px") // original font was too small
         .attr("transform", `translate(0, ${height - margin.bottom})`) // x-axis line was showing on top
@@ -146,10 +148,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       debugger
       const newX = x
-        .domain(
-          data
-            .sort(
-              this.checked
+        .domain(data.sort(this.checked
                 ? (a, b) => {
                     return b.calories - a.calories
                   }
@@ -182,8 +181,7 @@ document.addEventListener("DOMContentLoaded", () => {
       transition.select(".xText").call(xAxis).selectAll("g").delay(delay)
     }
 
-    // svg.append("g").call(xAxis)
-    // svg.append("g").call(yAxis)
+
     svg.node()
-  }
+  })
 })
