33aad7171df9b9681a158b74952acfd03fb89101
Daniel Ahn <iamdanahn@gmail.com>
2021-02-13 21:58:27 -0500

    Add ramen facts list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/index.scss"
 import "./styles/header.scss"
 import "./styles/graphs.scss"
+import "./styles/ramen_row.scss"
 
 import "./scripts/exercise"
 import "./scripts/ramen"
--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log(event)
+  const data = d3.csv("")
 })
