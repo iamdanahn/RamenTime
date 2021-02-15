7e13864572e4fd6be47555175a5db4dbe29ae8f4
Daniel Ahn <iamdanahn@gmail.com>
2021-02-14 23:50:16 -0500

    Modal works, messing with font

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import "./styles/reset.css"
 import "./styles/index.scss"
 import "./styles/header.scss"
 import "./styles/graphs.scss"
-import "./styles/ramen_row.scss"
 import "./styles/ramen_facts.scss"
+import "./styles/modal.scss"
 
 
 import "./scripts/exercise"
 import "./scripts/ramen"
 import "./scripts/footer"
+import "./scripts/modal"
