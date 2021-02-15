52d106631c20db9c6d9967ca7b2804b8d3a710f2
Daniel Ahn <iamdanahn@gmail.com>
2021-02-14 21:35:03 -0500

    Footer facts rotating

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles/index.scss"
 import "./styles/header.scss"
 import "./styles/graphs.scss"
 import "./styles/ramen_row.scss"
+import "./styles/ramen_facts.scss"
+
 
 import "./scripts/exercise"
 import "./scripts/ramen"
--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,3 +1,26 @@
+const factSpeed = 5 * 1000
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  const data = d3.csv("")
+  const data = d3
+    .csv(
+      "https://raw.githubusercontent.com/iamdanahn/RamenTime/ramenrow/assets/ramen_facts.csv",
+      (d) => {
+        return {
+          facts: d["Ramen Facts"],
+        }
+      },
+    )
+    .then((data) => {
+      function ramenFact(i) {
+        if (data.length > i) {
+          setTimeout(function () {
+            document.getElementById("ramen-fact").innerHTML = data[i].facts
+            ramenFact(++i)
+          }, factSpeed)
+        } else if (data.length === i) {
+          ramenFact(0)
+        }
+      }
+      ramenFact(0)
+    })
 })
