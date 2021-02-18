b0beb8c430c02f80e2a2f1509d84779e3a521702
Daniel Ahn <iamdanahn@gmail.com>
2021-02-18 16:44:45 -0500

    changed header to fa icons, add tim e bar

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,16 +1,37 @@
-const factSpeed = 5 * 1000
+const factSpeed = 7 * 1000
 
 document.addEventListener("DOMContentLoaded", (event) => {
+  
   const data = d3
-    .csv(
-      "https://raw.githubusercontent.com/iamdanahn/RamenTime/ramenrow/assets/ramen_facts.csv",
-      (d) => {
-        return {
-          facts: d["Ramen Facts"],
-        }
-      },
+  .csv(
+    "https://raw.githubusercontent.com/iamdanahn/RamenTime/ramenrow/assets/ramen_facts.csv",
+    (d) => {
+      return {
+        facts: d["Ramen Facts"],
+      }
+    },
     )
     .then((data) => {
+      function move() {
+        let i = 0;
+        if ( i === 0 ) {
+          i = 1;
+          const bar = document.getElementById("time-bar")
+          let width = 1;
+          let id = setInterval(frame, 70);
+
+          function frame() {
+            if (width >= 100) {
+              clearInterval(id);
+              i = 0;
+            } else {
+              width++
+              bar.style.width = `${width}%`
+            }
+          }
+        }
+      }
+      // rotating ramen facts
       function ramenFact(i) {
         if (data.length > i) {
           setTimeout(function () {
@@ -18,11 +39,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
             document.getElementById("ramen-fact").innerHTML = data[i].facts
             ramenFact(++i)
           }, factSpeed)
+          // move()
         } else if (data.length === i) {
           // creates a loop to start back at index 0
           ramenFact(0)
+          // move()
         }
       }
+      
+    
+
+
+
       ramenFact(0)
     })
 })
